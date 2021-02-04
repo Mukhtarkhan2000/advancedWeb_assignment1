715a88c20ce984b45bced897cbf6f7828db3b212
Mukhtarkhan2000 <m.nalibayev2900@gmail.com>
2021-02-04 21:42:15 +0600

    Add existing file

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddRecordComponent } from './add-record/add-record.component';
+import { ResultComponent } from './result/result.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddRecordComponent,
+    ResultComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
