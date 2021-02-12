866389af11922ad34455c5159ee3bb678bb0f4f9
Mukhtarkhan2000 <m.nalibayev2900@gmail.com>
2021-02-12 16:00:16 +0600

    Assignment2 commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AddRecordComponent } from './add-record/add-record.component';
-import { ResultComponent } from './result/result.component';
+import { HeaderComponent } from './header/header.component';
+import { LayoutComponent } from './layout/layout.component';
+import { AddRecordComponent } from './layout/add-record/add-record.component';
+import { ResultComponent } from './layout/result/result.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddRecordComponent,
-    ResultComponent
+    ResultComponent,
+    HeaderComponent,
+    FooterComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
